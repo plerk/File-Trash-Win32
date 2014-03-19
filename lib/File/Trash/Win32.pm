745e550c1319b25bf46cf5cd607de614e26b7e40
Graham Ollis <plicease@cpan.org>
2014-03-18 21:17:28 -0400

    Update Win32.pm
    comment possible resources

--- lib/File/Trash/Win32.pm
+++ lib/File/Trash/Win32.pm
@@ -7,4 +7,7 @@ use v5.10;
 # ABSTRACT: Trash files on Windows
 # VERSION
 
+# http://msdn.microsoft.com/en-us/library/windows/desktop/bb762164(v=vs.85).aspx
+# http://hg.hardcoded.net/send2trash/src/03a0c4986730f81bafc6b2dd9dec5c9f409965b3/send2trash/plat_win.py?at=default
+
 1;
